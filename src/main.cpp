afa073a89131249948697a7b5ea2ac5b75fca605
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-20 21:59:23 +0100

    Final version of main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
-#ifdef WIN32
-
 #include <windows.h>
-
-#endif
-
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <gl/GL.h>
 
-#ifdef WIN32
-
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
-
-#else
-
-int main()
-
-#endif
-
 {
 
 }
