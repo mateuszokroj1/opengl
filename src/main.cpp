a50d8d8c037e27f78f65e68eb7a48b06aad1b46e
Mateusz Okrój <mateuszokroj1@hotmail.com>
2022-03-04 16:41:16 +0100

    Architecture cleaned.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <windows.h>
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include <gl/GL.h>
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
